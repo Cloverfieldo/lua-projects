812b42ce3a11d0236ecd9043d2145a9749623110
Clover <cloverfieldor@gmail.com>
2020-11-25 13:15:13 -0300

    more stuff

--- animation/main.lua
+++ animation/main.lua
@@ -1,3 +1,4 @@
+testNumber = 0
 horizontalscale = 3
 rotation = 0
 x = 600
@@ -16,30 +17,33 @@ function love.update(dt)
     currentFrame = 1
   end
   -]]
-  local spriteSpeed = 10
-  if love.keyboard.isDown('d') then
-    local cool = x
-    spriteS = sprite.walkSide
-    --verticalScale = -3
-    horizontalscale = -3
-    coolT(spriteSpeed, dt)
-    --rotation = -180
-    x = x + 100 * dt
-  elseif love.keyboard.isDown('s') then
-    spriteS = sprite.walkDown
-    coolT(spriteSpeed, dt)
-    y = y + 100 * dt
-  elseif love.keyboard.isDown('a') then
-    spriteS = sprite.walkSide
-    horizontalscale = 3
-    x = x - 100 * dt
-    coolT(spriteSpeed, dt)
-  elseif love.keyboard.isDown('w') then
-    spriteS = sprite.walkUp
-    coolT(spriteSpeed, dt)
-    y = y - 100 * dt
-  else
-    currentFrame  = 1
+  testNumber = testNumber + 200 * dt
+  if testNumber >= 20 then
+    local spriteSpeed = 10
+    if love.keyboard.isDown('d') then
+      local cool = x
+      spriteS = sprite.walkSide
+      --verticalScale = -3
+      horizontalscale = -3
+      coolT(spriteSpeed, dt)
+      --rotation = -180
+      x = x + 100 * dt
+    elseif love.keyboard.isDown('s') then
+      spriteS = sprite.walkDown
+      coolT(spriteSpeed, dt)
+      y = y + 100 * dt
+    elseif love.keyboard.isDown('a') then
+      spriteS = sprite.walkSide
+      horizontalscale = 3
+      x = x - 100 * dt
+      coolT(spriteSpeed, dt)
+    elseif love.keyboard.isDown('w') then
+      spriteS = sprite.walkUp
+      coolT(spriteSpeed, dt)
+      y = y - 100 * dt
+    else
+      currentFrame  = 1
+    end
   end
 end
 
@@ -53,9 +57,21 @@ function coolT(speed,dt)
   if currentFrame >= 4 then
     currentFrame = 1
   end
-  
+
 end
 
 function love.keypressed(k)
-  simpleK = true
+  --simpleK = true
+  testNumber = 0
+  if k == 'd' then
+    spriteS = sprite.walkSide
+    horizontalscale = -3
+  elseif k == 'a' then
+    spriteS = sprite.walkSide
+    horizontalscale = 3
+  elseif k == 's' then
+    spriteS = sprite.walkDown
+  elseif k == 'w' then
+    spriteS = sprite.walkUp
+  end
 end
